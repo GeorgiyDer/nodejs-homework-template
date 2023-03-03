69cf7a60a63e88eaecb97d774b36b5adf6f52d27
GeorgiyDer <106014816+GeorgiyDer@users.noreply.github.com>
2023-03-03 11:45:28 +0200

    validation and schema errors fix

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,9 +1,9 @@
 const Joi = require("joi")
 
 const contactsSchema =  Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().required().messages({"any.required": "missing require name field"}),
+  email: Joi.string().email().required().messages({"any.required": "missing require email field"}),
+  phone: Joi.string().required().messages({"any.required": "missing require phone field"}),
 });
 
 module.exports = contactsSchema;
